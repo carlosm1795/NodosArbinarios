712c10de290132d14bfad3c5c7c3bf869e2e51df
carlosm1795 <carlosm1795@gmail.com>
2018-11-02 06:27:22 -0600

    Add files via upload

--- Arbol.java
+++ Arbol.java
@@ -5,6 +5,7 @@ package arbolesbinarios;
 public class Arbol {
     private NodoArbol raiz;
     
+    
     public Arbol(){
         raiz = null;
     }
@@ -29,6 +30,8 @@ public class Arbol {
         ayudantePreorden( nodo.nodoIzq );
         ayudantePreorden( nodo.nodoDer );
     }
+ 
+
     
     public void recorridoInorden(){
         ayudanteInorden( raiz );
@@ -41,6 +44,7 @@ public class Arbol {
         }
         ayudanteInorden( nodo.nodoIzq );
         System.out.printf( "%d ", nodo.datos );
+        
         ayudanteInorden( nodo.nodoDer );
   
     }
@@ -54,6 +58,68 @@ public class Arbol {
         ayudantePostorden( nodo.nodoIzq );
         ayudantePostorden( nodo.nodoDer );
         System.out.printf( "%d ", nodo.datos );
+    }
+    
+    public void eliminarNodo(int pDato){
+        boolean nodoEncontrado = buscarNodo(raiz,pDato);
+        if(nodoEncontrado == true){
+            if(pDato>raiz.datos){
+                deleteNodo(raiz,raiz.nodoDer,pDato);
+            }else{
+                    deleteNodo(raiz,raiz.nodoIzq,pDato);
+            }
+            
+        }else{
+            System.out.println("Numero no encontrado" );
+        }
+        
+    }
+    
+    public void deleteNodo(NodoArbol nodo,NodoArbol nodoSiguiente,int pDato){
+        if (nodo == null){
+            return;
+        }
+        if (pDato == nodoSiguiente.datos){
+            if(nodoSiguiente.nodoDer == null && nodoSiguiente.nodoIzq == null){
+                if(nodo.nodoDer == nodoSiguiente){
+                    nodo.nodoDer = null;
+                }else{
+                        nodo.nodoIzq = null;
+                }
+                return;
+            }
+            
+        }else if (nodoSiguiente.datos>pDato){
+             deleteNodo(nodoSiguiente,nodoSiguiente.nodoIzq,pDato);
+        }else{
+             deleteNodo(nodoSiguiente,nodoSiguiente.nodoDer,pDato);
+        }
+    }
+    
+    public int getLeafCount(NodoArbol node)  
+    { 
+        if (node == null) 
+            return 0; 
+        if (node.nodoDer == null && node.nodoIzq == null) 
+            return 1; 
+        else
+            return getLeafCount(node.nodoDer) + getLeafCount(node.nodoIzq)+1; 
+    } 
+    public void search(){
+        System.out.println(buscarNodo(raiz,10));
+    }
+    public boolean buscarNodo(NodoArbol node,int pDato){
+        if (node == null){
+            return false;
+        }
+        if (pDato == node.datos){
+            return true;
+        }else if (node.datos>pDato){
+            return buscarNodo(node.nodoIzq,pDato);
+        }else{
+            return buscarNodo(node.nodoDer,pDato);
+        }
+        
     }
     
     
--- ArbolesBinarios.java
+++ ArbolesBinarios.java
@@ -12,21 +12,34 @@ public class ArbolesBinarios {
         Arbol arbol = new Arbol();
         int valor;
         Random numeroAleatorio = new Random();
+        int valoresAInsertar = 10;
         System.out.println( "Insertando los siguientes valores: " );
-        for ( int i = 1; i <= 10; i++ ){
-            valor = numeroAleatorio.nextInt( 100 );
+        /*for ( int i = 1; i <= valoresAInsertar; i++ ){
+            valor = numeroAleatorio.nextInt( 11 );
             System.out.print( valor + " " );
             arbol.insertarNodo( valor );
-        }
-        System.out.println ( "\n\nRecorrido preorden" );
-        arbol.recorridoPreorden();
+        }*/
+        arbol.insertarNodo(6);
+        arbol.insertarNodo(2);
+        arbol.insertarNodo(10);
+        arbol.insertarNodo(11);
+        System.out.println("");
+        //System.out.println ( "\n\nRecorrido preorden" );
+        //arbol.recorridoPreorden();
         
-        System.out.println ( "\n\nRecorrido inorden" );
+        //System.out.println ( "\n\nRecorrido inorden" );
         arbol.recorridoInorden();
         
-        System.out.println ( "\n\nRecorrido postorden" );
-        arbol.recorridoPostorden();
-        System.out.println();
+        //System.out.println ( "\n\nRecorrido postorden" );
+        //arbol.recorridoPostorden();
+        //System.out.println();
+        
+        //arbol.verificarNodo(valoresAInsertar);
+        arbol.search();
+        arbol.eliminarNodo(10);
+        
+        
+        arbol.recorridoInorden();
     }
     
 }
